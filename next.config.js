257ed4bdbad86f93823497fbf34a0f4eeff060b8
ThalusA <15985204+ThalusA@users.noreply.github.com>
2023-09-02 19:39:01 +0200

    Add PWA configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone"
+    output: "standalone",
+    reactStrictMode: true,
 }
+
+module.exports = withPWA(nextConfig)
 
 module.exports = nextConfig
